b7a35e8e8068dde0869806133d577287884f3ea8
shohaib1996 <khanshohaibhossain@gmail.com>
2025-01-05 20:24:35 +0600

    authentication part and home page done

---
+++ src/components/UI/AuthButton/AuthButton.tsx
@@ -0,0 +1,33 @@
+
+
+import { getUserInfo, removeUser } from "@/services/auth.services";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+const AuthButton = () => {
+  const userInfo = getUserInfo();
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    removeUser();
+    router.refresh();
+  };
+  console.log(userInfo);
+  
+  return (
+    <>
+      {userInfo?.email ? (
+        <Button color="error" onClick={handleLogOut}>
+          Logout
+        </Button>
+      ) : (
+        <Button component={Link} href="/login">
+          Login
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default AuthButton;
